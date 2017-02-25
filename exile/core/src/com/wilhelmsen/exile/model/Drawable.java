7659f14f6e9518a4add1f75bb0e914c22479c430
Harald Floor Wilhelmsen <haraldfw@gmail.com>
2017-02-25 17:26:36 +0100

    A new project with NOTHING in it
    :D

---
+++ exile/core/src/com/wilhelmsen/exile/model/Drawable.java
@@ -0,0 +1,7 @@
+package com.wilhelmsen.exile.model;
+
+/**
+ * Created by Harald on 24.02.2017.
+ */
+public interface Drawable {
+}
---
+++ exile/core/src/com/wilhelmsen/exile/model/player/Player.java
@@ -0,0 +1,8 @@
+package com.wilhelmsen.exile.model.player;
+
+/**
+ * Created by Harald on 24.02.2017.
+ */
+public abstract class Player {
+
+}
